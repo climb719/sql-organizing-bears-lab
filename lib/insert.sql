d01af6d13ce4748d9c74bf35184da1275ed1275d
climb719 <mary.oldweiler@gmail.com>
2021-03-09 11:47:45 -0500

    commit tree 9105f19eaff3156f810cfbac567366f7e4e29752

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 5, 'M', 'Brown', 'Sweet', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 2, 'M', 'Black', 'Wild', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 10, 'F', 'Cinnamon', 'Fierce', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 11, 'M', 'Brown', 'Gentle', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 4, 'F', 'Blond', 'Tame', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 15, 'M,', 'Brown', 'Grouchy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 19, 'F', 'Black', 'Friendly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 12, 'F', 'Dark Brown', 'Ferocious', 0);
